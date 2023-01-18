79b6f16790b7b9bb572b06d78d1f218578de610e
ying <34480@ma-web.nl>
2023-01-18 15:30:36 +0100

    Modified files

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+let game;
+function createPlayers(amountOfPlayers) {
+    game.start(amountOfPlayers);
+}
+function init()
+{
+    game = new Game();
+}
+
 class Player
 {
     constructor(index)
     {
         this.index = index;
         this.atTile = 0;
+        console.log(this.atTile);
         this.pawn = document.getElementsByClassName("pawn" + index)[0];
         this.pawn.style.display = "block";
     }
@@ -30,9 +40,13 @@ class Game
 
         this.tiles = [];
         this.players = [];
+        console.log(this.tiles)
+        console.log(this.players)
         this.playerTurn = 0;
+        console.log(this.playerTurn)
         this.setupBoard();
     }
+
     setupBoard()
     {   
         let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
@@ -56,56 +70,93 @@ class Game
             this.tiles.push(tile);
 
         }
-        this.setupGotos(); 
+        this.setupGotos();
     }
 
     setupGotos()
-    {
+    {   
         let goto = [[6, 14], [16, 4], [17, 23], [27, 33], [29, 10], [38, 43], [39, 20], [45, 34]];
         for (var i = 0; i < goto.length; i++)
         {
             let element = goto[i];
             let start = element[0] - 1;
             let end = element[1] - 1;
-            
             let tile = this.tiles[start];
             tile.goto = end;
-            console.log(tile)
         }
     }
-    start(amountOfPlayers)
-    {
 
+    start(amountOfPlayers)
+    {   
+        this.selectplayersDiv.style.display = "none";
+        this.winnerDiv.style.display = "none";
+        for (let i = 0; i < 4; i++) {
+            document.getElementsByClassName("pawn" + i)[0].style.display = "none";
+        }
+        for (let i = 0; i < amountOfPlayers; i++) {
+            this.players.push(new Player(i));
+        }
+        this.playerTurn = -1;
+        this.moveToNextPlayer();
     }
+
     moveToNextPlayer()
     {
-
+        this.playerTurn = (this.playerTurn + 1) % this.players.length;
+        this.playerturnDiv.textContent = "Player " + (this.playerTurn + 1) + "'s turn.";
+        this.rollDiv.style.display = "block";
+        this.draw();
     }
+
     draw()
     {
-
+        for (let i = 0; i < this.players.length; i++) {
+            this.setPawn(i, this.players[i].atTile);
+        }
     }
-    roll()
+
+    setPawn(playerI, atTile) {
+        let player = this.players[playerI];
+        player.atTile = atTile;
+        let tile = this.tiles[atTile];
+        player.pawn.style.left = tile.div.style.left;
+        player.pawn.style.top = tile.div.style.top;
+    }
+
+    roll() 
     {
-
+        let roll = Math.floor(Math.random() * 6) + 1;
+        console.log(roll)
+        this.rollDiv.style.backgroundImage = "url(img/dice" + roll + ".png)";
+        let player = this.players[this.playerTurn];
+        let atTile = player.atTile;
+        console.log(player);
+        atTile = (atTile + roll) % this.tiles.length;
+        if (atTile >= this.tiles.length-1) {
+            this.winnerDiv.textContent = "Player " + (this.playerTurn + 1) + " wins!";
+            this.winnerDiv.style.display = "block";
+        } 
+        else {
+            let tile = this.tiles[atTile];
+            if (tile.goto !== -1) {
+                atTile = tile.goto;
+            }
+            this.setPawn(this.playerTurn, atTile);
+            this.draw();
+            this.moveToNextPlayer();
+        }
     }
-    setPawn(playerI, atTile)
-    {
-
-    }
+    
     makeBoardDiv(x,y,tileDisplayNumber)
     {
         let div = document.createElement("div");
         div.className = "tile";
         div.style.left = x + "px";
         div.style.top = y + "px";
-        div.textContent = tileDisplayNumber;       
+        div.textContent = tileDisplayNumber;      
         this.boardDiv.appendChild(div);
 
         return div;
     }
-}
-function init()
-{
-    let game = new Game();
+
 }
